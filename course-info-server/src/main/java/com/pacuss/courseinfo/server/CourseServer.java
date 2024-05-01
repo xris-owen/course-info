c1305541664bfc38cdbb1ce1eddc5020de92b12a
Chukwudi Okolo <chydan14@gmail.com>
2024-05-02 00:46:41 +0100

    Added course-info-server module to expose API endpoints to the client.

---
+++ course-info-server/src/main/java/com/pacuss/courseinfo/server/CourseServer.java
@@ -0,0 +1,24 @@
+package com.pacuss.courseinfo.server;
+
+import com.pacuss.courseinfo.repository.CourseRepository;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+
+public class CourseServer {
+    private static final Logger LOG = LoggerFactory.getLogger(CourseResource.class);
+    private static final String BASE_URI = "http://localhost:8080";
+
+    public static void main(String[] args) {
+        LOG.info("Starting HTTP Server...");
+        CourseRepository courseRepository = CourseRepository.openCourseRepository("./courses.db");
+        ResourceConfig config = new ResourceConfig().register(new CourseResource(courseRepository));
+
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+
+
+    }
+}
